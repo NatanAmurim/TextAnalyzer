f42195802847a80abff6c8e458209c8586f2e41e
natanamurim <natanamurim1998@gmail.com>
2025-08-12 17:27:33 -0300

    fix: alteração de namespace para nova localização de classe de erros.

--- TextAnalyzer.Api/Controllers/TextAnalyzerController.cs
+++ TextAnalyzer.Api/Controllers/TextAnalyzerController.cs
@@ -1,19 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using TextAnalyzer.Api.Application.CommandsAndQueries.TextAnalyzer.Commands;
-using TextAnalyzer.Api.Application.DTOs;
+using TextAnalyzer.Api.Application.Common;
 using TextAnalyzer.Api.Application.Handlers;
 
 namespace TextAnalyzer.Api.Controllers
-{    
+{
     [ApiController]
     [Route("api/text-analyzer")]
     public class TextAnalyzerController(TextAnayzerHandler textAnayzerService) : ControllerBase
-    {        
+    {
         [HttpPost]
         public IActionResult Post([FromBody] TextAnalyzerCommand textAnalyzerCommand)
-        {            
+        {
 
-            if (string.IsNullOrEmpty(textAnalyzerCommand.Text))            
+            if (string.IsNullOrEmpty(textAnalyzerCommand.Text))
                 return BadRequest(new ErrorResult("O valor de text não pode ser nulo."));
 
             var result = textAnayzerService.AnalyzeText(textAnalyzerCommand.Text);
